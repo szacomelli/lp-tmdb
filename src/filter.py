9ed250177ecbb449d95626ae28b20194e3e5933b
Sílvio E. S. Zacomelli <zacomellisilvio@gmail.com>
2024-10-04 21:40:52 -0300

    filter first hypothesis done

---
+++ src/filter.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+#global variables
+file_path = "data/TMDB_tv_dataset_v3.csv"
+file = pd.read_csv(file_path, delimiter=",")
+
+def filter_first(votes_minimum):
+    data = file[['name', 'vote_count', 'vote_average', 'number_of_seasons', 'number_of_episodes']].replace(to_replace=0, value=np.nan).dropna().index
+    file_votes = file.loc[data]
+    return (file_votes[file_votes['vote_count'] > votes_minimum]).copy()
+
+def filter_second():
+    data = file[['name', 'vote_count', 'vote_average', 'popularity', 'genres', 'networks']].replace(to_replace=0, value=np.nan).dropna().index
+    return (file.loc[data]).copy()
+
+def filter_third():
+    data = file[['name', 'vote_count', 'vote_average', 'popularity', 'genres', 'networks']].replace(to_replace=0, value=np.nan).dropna().index
+    return (file.loc[data]).copy()
+
+
+
+
+
+
+filter_first(0).to_csv("abacate.csv")
