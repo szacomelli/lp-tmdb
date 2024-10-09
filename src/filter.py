e087b67d0257f1c0731c4914a976cfe88e1d438f
Sílvio E. S. Zacomelli <zacomellisilvio@gmail.com>
2024-10-09 08:52:29 -0300

    filter_second and silvio_hypotheses done

--- src/filter.py
+++ src/filter.py
@@ -5,13 +5,14 @@ import numpy as np
 file_path = "data/TMDB_tv_dataset_v3.csv"
 raw_file = pd.read_csv(file_path, delimiter=",")
 
-def filter_first(votes_minimum : int):
+def filter_first(votes_minimum : int) -> pd.DataFrame:
     data = raw_file[['name', 'vote_count', 'vote_average', 'number_of_seasons', 'number_of_episodes']].replace(to_replace=0, value=np.nan).dropna().index
     file_votes = raw_file.loc[data]
     return (file_votes[file_votes['vote_count'] > votes_minimum]).copy()
 
-def filter_second(series_minimum : int):
-    data_index = raw_file[['name', 'vote_count', 'vote_average', 'popularity', 'genres', 'networks']].replace(to_replace=0, value=np.nan).dropna().index
+def filter_second(series_minimum : int, date_max : list[int]=[0, 9999]) -> pd.DataFrame:
+    d_file = raw_file[['name', 'vote_count', 'vote_average', 'popularity', 'genres', 'networks', 'first_air_date']].replace(to_replace=0, value=np.nan).dropna()
+    data_index = d_file[[int(item[0]) >= date_max[0] and int(item[0]) <= date_max[1] for item in d_file['first_air_date'].str.split('-')[:].tolist()]].index
     flt_file = (raw_file.loc[data_index]).copy()
     flt_file['genres'] = flt_file['genres'].str.split(", ")
     flt_file['networks'] = flt_file['networks'].str.split(", ")
@@ -21,7 +22,7 @@ def filter_second(series_minimum : int):
     flt_file = flt_file[flt_file['networks'].isin(net_list)]
     return flt_file.copy()
 
-def filter_third(votes_minimum : int):
+def filter_third(votes_minimum : int) -> pd.DataFrame:
     data = raw_file[['name', 'vote_count', 'vote_average', 'popularity', 'networks']].replace(to_replace=0, value=np.nan).dropna().index
     file_votes = raw_file.loc[data]
     return (file_votes[file_votes['vote_count'] > votes_minimum]).copy()
