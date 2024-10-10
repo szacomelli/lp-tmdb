8b5b21fe3d0e3d020fd166222fc515d196d663d2
Sílvio E. S. Zacomelli <zacomellisilvio@gmail.com>
2024-10-10 01:11:54 -0300

    commenting

--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@ import dilmar_hypothesis as dm
 import silvio_hypothesis as sv
 import filter
 
+#Hypothesis 1
 
-
+#Hypothesis 2
 sv.most_frequent_genre(10, 100)
 sv.most_popular_genre(10, 100)
 sv.most_voted_genre(10, 100)
@@ -16,5 +17,6 @@ sv.most_frequent_genre(10, 100, [2023,2024])
 sv.most_popular_genre(10, 100, [2023,2024])
 sv.most_voted_genre(10, 100, [2023,2024])
 
+#Hypothesis 3
 dm.dilmar_hypotesis(10, 100)
 
